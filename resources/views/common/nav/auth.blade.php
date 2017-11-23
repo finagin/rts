ab51b38f0e3e304f51d43d33d054d9ece5f4f1c6
Igor Finagin <Igor@Finag.in>
2017-11-23 13:03:59 +0300

    Shorten HTML

--- resources/views/common/nav/auth.blade.php
+++ resources/views/common/nav/auth.blade.php
@@ -5,16 +5,14 @@
 @else
     @includeIf('common.nav.right')
     <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"
-           aria-haspopup="true">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
             {{ Auth::user()->name }} <span class="caret"></span>
         </a>
 
         <ul class="dropdown-menu">
             <li>
                 <a href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     Logout
                 </a>
 
--- resources/views/common/nav/right.blade.php
+++ resources/views/common/nav/right.blade.php
@@ -1,7 +1,6 @@
 {{-- Right Side Of Navbar --}}
 <li class="dropdown">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"
-       aria-haspopup="true">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
         Пользователи <span class="caret"></span>
     </a>
 
